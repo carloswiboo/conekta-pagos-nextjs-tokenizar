43fea0987c6c66d9266cc787d2e698cc14986806
Carlos Francisco Estrada Garcidueñas <carlos@wiboo.com.mx>
2023-10-11 18:21:06 -0600

    Ya quedó lista la prueba de conekta

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          src="https://pay.conekta.com/v1.0/js/conekta-checkout.min.js"
+          strategy="beforeInteractive"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
